95f7139d7f9160148a58189b158eb08ac7072110
JanisHuser <huser.janis00@gmail.com>
2024-10-05 19:46:27 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, db
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("Database initialized.")
+
+init_db()
 
 if __name__ == "__main__":
     app.run()
